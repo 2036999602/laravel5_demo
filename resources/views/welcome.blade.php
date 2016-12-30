723f60f9d1d8705def8fbf41f9059668cc28d5b5
txb <txb@qq.com>
2016-12-30 15:57:37 +0800

    bug 333

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 59999999</div>
+                <div class="title">Laravel333</div>
             </div>
         </div>
     </body>
