8bc3d5dcc0b4ab62baf698efd10b7fbaca5bc23a
txb <txb@qq.com>
2016-12-30 16:06:50 +0800

    master welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel333</div>
+                <div class="title">master file</div>
             </div>
         </div>
     </body>
